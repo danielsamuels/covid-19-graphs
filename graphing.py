574f4a18d3bf356f244e1fbf22f754dd4acdc435
Daniel Samuels <daniel.samuels@bromium.com>
2020-04-07 18:31:42 +0100

    Change to a class

--- graphing.py
+++ graphing.py
@@ -1,5 +1,9 @@
+import argparse
 import csv
+import logging
 import os
+import sys
+from collections import ChainMap
 from datetime import datetime
 from operator import itemgetter
 from pathlib import Path
@@ -20,114 +24,151 @@ countries = {
         'state': [''],
     }
 }
-active_country = 'UK'
+
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+logger = logging.getLogger(__name__)
 
 
-def row_filter(active_country: str, row: dict) -> bool:
-    country = countries[active_country]
-    if 'Country_Region' in row:
-        return row['Country_Region'] in country['country'] and row['Province_State'] in country['state']
+class Generator:
+    def __init__(self, config):
+        self.config = config
+        self.active_country: str = config.country
+        self.data: list = self.get_data()
 
-    if 'Country/Region' in row:
-        return row['Country/Region'] in country['country'] and row['Province/State'] in country['state']
+    def row_filter(self, row: dict) -> bool:
+        country = countries[self.active_country]
+        if 'Country_Region' in row:
+            return row['Country_Region'] in country['country'] and row['Province_State'] in country['state']
 
-    raise Exception(f'Unable to process row, available keys: {row.keys()}')
+        if 'Country/Region' in row:
+            return row['Country/Region'] in country['country'] and row['Province/State'] in country['state']
 
+        raise Exception(f'Unable to process row, available keys: {row.keys()}')
 
-def process_file(filename):
-    date = datetime.strptime(filename.name.rstrip('.csv'), '%m-%d-%Y')
-    confirmed = deaths = recovered = 0
+    def process_file(self, filename):
+        date = datetime.strptime(filename.name.rstrip('.csv'), '%m-%d-%Y')
+        confirmed = deaths = 0
 
-    # Pull out the United Kingdom data for this day.
-    with open(filename, encoding='utf-8-sig') as csvfile:
-        reader = csv.DictReader(csvfile)
-        try:
-            data = next(row for row in reader if row_filter(active_country, row))
-            confirmed, deaths, recovered = int(data['Confirmed'] or 0), int(data['Deaths'] or 0), int(
-                data['Recovered'] or 0)
-        except StopIteration:
-            print(f'No {active_country} data in {filename}')
+        # Pull out the United Kingdom data for this day.
+        with open(filename, encoding='utf-8-sig') as csvfile:
+            reader = csv.DictReader(csvfile)
+            try:
+                data = next(row for row in reader if self.row_filter(row))
+                confirmed, deaths = int(data['Confirmed'] or 0), int(data['Deaths'] or 0)
+            except StopIteration:
+                logger.debug(f'No {self.active_country} data in {filename}')
 
-    return date, confirmed, deaths, recovered
+        return date, confirmed, deaths
 
+    def get_data(self):
+        # Pull the data from each CSV file.
+        directory = Path('.') / 'COVID-19' / 'csse_covid_19_data' / 'csse_covid_19_daily_reports'
+        files = directory.glob('*.csv')
+        time_series = list(map(self.process_file, files))
+        time_series.sort(key=itemgetter(0))
 
-def get_data():
-    # Pull the data from each CSV file.
-    directory = Path('.') / 'COVID-19' / 'csse_covid_19_data' / 'csse_covid_19_daily_reports'
-    files = directory.glob('*.csv')
-    time_series = list(map(process_file, files))
-    time_series.sort(key=itemgetter(0))
+        return time_series
 
-    return time_series
+    def generate_basic_data_graph(self):
+        self.write_graph('cases', self.data)
 
+    def generate_rate_delta_graph(self):
+        """How are the numbers changing each day?"""
+        deltas = []
+        p_date = p_confirmed = p_deaths = None
+        for index, data in enumerate(self.data):
+            if not p_date:
+                p_date, p_confirmed, p_deaths = data
+                continue
 
-def generate_basic_data_graph(time_series):
-    time_shift = 14
-    write_graph(
-        time_series,
-        f"{active_country}-{f'shifted-{time_shift}-days' if time_shift else 'unshifted'}.png",
-        title=f'COVID-19 cases in {active_country}, with the deaths shifted by {time_shift} days',
-        log_scale=False,
-        time_shift=time_shift,
-        truncate_x_axis=False,
-    )
+            date, confirmed, deaths = data
+            deltas.append((date, confirmed - p_confirmed, deaths - p_deaths))
+            p_date, p_confirmed, p_deaths = data
 
+        # Remove confirmed
+        # deltas = [(date, 0, deaths) for date, confirmed, deaths in deltas]
 
-def generate_rate_delta_graph(time_series):
-    """How are the numbers changing each day?"""
-    deltas = []
-    p_date = p_confirmed = p_deaths = p_recovered = None
-    for index, data in enumerate(time_series):
-        if not p_date:
-            p_date, p_confirmed, p_deaths, p_recovered = data
-            continue
+        self.write_graph('deltas', deltas)
 
-        date, confirmed, deaths, recovered = data
-        deltas.append((date, confirmed - p_confirmed, deaths - p_deaths))
-        p_date, p_confirmed, p_deaths, p_recovered = data
+    def generate_filename(self, graph_type: str):
+        return f"{self.active_country}-{graph_type}-{f'shifted-{self.config.shift}-days' if self.config.shift else 'unshifted'}{f' (log)' if self.config.log else ''}.png"
 
-    time_shift = 14
-    write_graph(
-        deltas,
-        f"{active_country}-deltas-{f'shifted-{time_shift}-days' if time_shift else 'unshifted'}.png",
-        title=f'Deltas in confirmed and deaths, with the deaths shifted by {time_shift} days',
-        time_shift=14,
-    )
+    def generate_title(self, graph_type: str):
+        if self.config.shift:
+            return f'{graph_type.title()} in confirmed and deaths, with the deaths shifted by {self.config.shift} days'
 
+        return f'{graph_type.title()} in confirmed and deaths'
 
-def write_graph(
-    time_series,
-    output_filename, *,
-    title: str = None,
-    log_scale: bool = False,
-    time_shift: int = None,
-    truncate_x_axis: bool = False
-):
-    if not time_shift:
-        time_shift = 0
+    def write_graph(
+        self,
+        graph_type,
+        time_series
+    ):
+        time_shift = self.config.shift
 
-    x = [t[0] for t in time_series]
-    if time_shift and truncate_x_axis:
-        x = x[:-time_shift]
+        if not time_shift:
+            time_shift = 0
 
-    confirmed = [t[1] for t in time_series]
-    deaths = [t[2] for t in time_series]
+        x = [t[0].strftime('%d %b') for t in time_series]
+        confirmed = [t[1] for t in time_series]
+        deaths = [t[2] for t in time_series]
 
-    fig = go.Figure(data=[
-        go.Bar(name='Confirmed Cases', x=x, y=confirmed),
-        go.Bar(name='Deaths', x=x, y=deaths[time_shift:]),
-    ])
+        if self.config.any_case:
+            # Find the first index with data in either category
+            offset = next(index for index, (c, d) in enumerate(zip(confirmed, deaths)) if c + d > 0)
+        else:
+            # Find the first index with data in both categories
+            offset = next(index for index, (c, d) in enumerate(zip(confirmed, deaths)) if c > 0 and d > 0)
 
-    if log_scale:
-        fig.update_layout(yaxis_type='log')
+        x = x[offset:]
+        confirmed = confirmed[offset:]
+        deaths = deaths[offset:]
 
-    if title:
-        fig.update_layout(title=title)
+        fig = go.Figure(data=[
+            go.Bar(name='Confirmed Cases', x=x, y=confirmed),
+            go.Bar(name='Deaths', x=x, y=deaths[time_shift:]),
+        ], layout=go.Layout(
+            title=self.generate_title(graph_type),
+            yaxis=dict(
+                type='log' if self.config.log else 'linear',
+            ),
+            xaxis=dict(
+                # showgrid=True,
+                # type='category',
+            ),
+        ))
 
-    fig.write_image(f'images/{output_filename}', width=1100, height=628, scale=2)
+        output_filename = self.generate_filename(graph_type)
+        logger.info(f'Wrote images/{output_filename}')
+        fig.write_image(f'images/{output_filename}', width=1100, height=628, scale=2)
 
 
 if __name__ == '__main__':
-    data = get_data()
-    # generate_basic_data_graph(data)
-    generate_rate_delta_graph(data)
+    parser = argparse.ArgumentParser(description='Generate COVID-19 graphs.')
+    parser.add_argument("-s", "--shift", default=14, type=int, help="Number of days to shift deaths by.")
+    parser.add_argument("-l", "--log", action='store_true', help="Whether to use a log-scale.")
+    parser.add_argument("-c", "--country", default='UK', type=str, choices=countries.keys(), help="Which country to render data for.")
+    parser.add_argument("-a", "--any-case", action='store_true', help="Show columns where there are _any_ cases, rather than needing at least one confirmed case and one death.")
+
+    parser.add_argument('-b', '--basic', action='append_const', dest='graphs', const='basic', help='Generate the basic data graph.')
+    parser.add_argument('-d', '--delta', action='append_const', dest='graphs', const='delta', help='Generate the delta trends graph.')
+
+    parser.add_argument('-v', '--verbose', action='store_true')
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        logger.setLevel(logging.NOTSET)
+
+    if args.graphs is None:
+        args.graphs = ['basic']
+
+    logger.info(f'Starting graphing with args: {args}')
+
+    generator = Generator(args)
+
+    if 'basic' in args.graphs:
+        generator.generate_basic_data_graph()
+
+    if 'delta' in args.graphs:
+        generator.generate_rate_delta_graph()
